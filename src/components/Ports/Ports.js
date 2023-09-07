351da3749553cb1dc42a9b07a5ea192b6c78cbe7
Prashanth <56287217+Prashanth1856@users.noreply.github.com>
2023-09-07 09:05:47 +0530

    Update Ports.js

--- src/components/Ports/Ports.js
+++ src/components/Ports/Ports.js
@@ -3,9 +3,9 @@ import "./index.css";
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 
-const redImg = "/Port-2.png";
-const greenImg = "/Port-4.png";
-const connector = "/Connectors.png";
+const redImg = "./Port-2.png";
+const greenImg = "./Port-4.png";
+const connector = "./Connectors.png";
 
 const Ports = () => {
   const [data, setData] = useState(null);
